e0c7fbd4dccf41e8c58ab74e5a56462504c2c199
donpele <ikpe388@gmail.com>
2024-01-07 06:05:10 -0800

    0x02. Python - import & modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,8 @@
-#!/useribin/python3
-
-if _name_ == " _main_":
-    """print the sum of 1 and 2. """
+#!/usr/bin/python3
+if __name__ == "__main__":
     from add_0 import add
 
     a = 1
     b = 2
-    print("{} + {} = . format (a, b, add(a, b)))
+
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
