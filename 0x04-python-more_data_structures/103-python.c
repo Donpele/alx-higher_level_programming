4942caca4dc09a876124155fa6eadc256879a3cc
donpele <ikpe388@gmail.com>
2024-01-10 09:59:40 -0800

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <Python.h>
+#include <object.h>
+#include <listobject.h>
+#include <bytesobject.h>
 
 /**
  * print_python_bytes - function that Prints bytes information
